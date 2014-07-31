db887ddd72caa0a218f4a7778d05e854b606a553
qimmer <twicfall@gmail.com>
2014-08-01 00:00:25 +0200

    ...

---
+++ Assets/Scripts/Block.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Block : MonoBehaviour {
+
+    float DamageMultiplier = 0.1f;
+
+    public float Health;
+    public float Armor;
+    public float Weight;
+
+	// Use this for initialization
+	void Start () {
+        Health = 1.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public virtual void OnCollisionEnter(Collision col)
+    {
+        Health -= (col.relativeVelocity.magnitude * DamageMultiplier) / Armor;
+    }
+}
