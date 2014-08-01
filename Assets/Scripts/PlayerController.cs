4698577106385f0244526cf3536a55158d07a3fb
qimmer <twicfall@gmail.com>
2014-08-01 02:31:01 +0200

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	.gitignore

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,32 +1,37 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : MonoBehaviour
+{
 
-	public KeyCode upKey;
-	public KeyCode downKey;
-	public KeyCode leftKey;
-	public KeyCode rightKey;
+    public int PlayerIndex;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void FixedUpdate () 
-	{
-		if (Input.GetAxis("Vertical") > 0 && rigidbody2D.velocity.y < 2)
-		{
-			gameObject.rigidbody2D.AddForce(new Vector2(0, (Input.GetAxis("Vertical"))) * 25);
-			//Mathf.Clamp(rigidbody2D.velocity.y, -5, 2);
-			print(rigidbody2D.velocity.y);
-		}
+    // Use this for initialization
+    void Start()
+    {
 
-		gameObject.rigidbody2D.AddForce(new Vector2(Input.GetAxis("Horizontal"),0) * 25);
-		if (Input.GetKey(downKey))
-		{
+    }
 
-		}
-	}
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        if (gameObject.rigidbody2D.velocity.y < 6 && Input.GetButton("Player" + PlayerIndex + "_Throttle"))
+        {
+            gameObject.rigidbody2D.AddForce(new Vector2(0.0f, 600.0f));
+        }
+
+        if (!Input.GetButton("Player" + PlayerIndex + "_Throttle"))
+            gameObject.rigidbody2D.AddForce(new Vector2(0.0f, 200.0f));
+
+        gameObject.rigidbody2D.AddForce(new Vector2(Input.GetAxis("Player" + PlayerIndex + "_Axis") * 140.0f, 0));
+
+        gameObject.rigidbody2D.velocity = new Vector2(Mathf.Clamp(gameObject.rigidbody2D.velocity.x * 0.93f, -25, 25), Mathf.Clamp(gameObject.rigidbody2D.velocity.y, -4, 12));
+
+        if (Input.GetButton("Player" + PlayerIndex + "_Detach"))
+        {
+            transform.FindChild("Trigger").gameObject.GetComponent<RopeTrigger>().ReleaseBox();
+        }
+    }
+
 }
