dd30a9809fcab50db3383223c365702b0bb66b9e
qimmer <twicfall@gmail.com>
2014-08-01 02:12:42 +0200

    Alot!

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    public int PlayerIndex;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () 
+	{
+
+        if (gameObject.rigidbody2D.velocity.y < 6 && Input.GetButton("Player" + PlayerIndex + "_Throttle"))
+        {
+            gameObject.rigidbody2D.AddForce(new Vector2(0.0f, 600.0f));
+        }
+
+        if (!Input.GetButton("Player" + PlayerIndex + "_Throttle"))
+            gameObject.rigidbody2D.AddForce(new Vector2(0.0f, 200.0f));
+
+        gameObject.rigidbody2D.AddForce(new Vector2(Input.GetAxis("Player" + PlayerIndex + "_Axis") * 140.0f, 0));
+
+        gameObject.rigidbody2D.velocity = new Vector2(Mathf.Clamp(gameObject.rigidbody2D.velocity.x * 0.93f, -25, 25), Mathf.Clamp(gameObject.rigidbody2D.velocity.y, -4, 12));
+
+        if( Input.GetButton("Player" + PlayerIndex + "_Detach") )
+        {
+            transform.FindChild("Trigger").gameObject.GetComponent<RopeTrigger>().ReleaseBox();
+        }
+	}
+
+}
---
+++ Assets/Scripts/RopeTrigger.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RopeTrigger : MonoBehaviour {
+
+    GameObject attachedObject = null;
+    float lastGrabTime = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Block" && attachedObject == null && Time.time > lastGrabTime + 5.0f)
+        {
+            if (col.gameObject.GetComponent<Block>().Owner != null)
+                return;
+
+            col.gameObject.GetComponent<DistanceJoint2D>().enabled = true;
+            col.gameObject.GetComponent<DistanceJoint2D>().connectedBody = rigidbody2D;
+            col.gameObject.GetComponent<Block>().Owner = gameObject;
+            attachedObject = col.gameObject;
+            lastGrabTime = Time.time;
+        }
+    }
+
+    public void ReleaseBox()
+    {
+        if( attachedObject != null )
+        {
+            attachedObject.GetComponent<Block>().Owner = null;
+            attachedObject.GetComponent<DistanceJoint2D>().connectedBody = null;
+            attachedObject.GetComponent<DistanceJoint2D>().enabled = false;
+            attachedObject = null;
+            lastGrabTime = Time.time;
+        }
+    }
+}
